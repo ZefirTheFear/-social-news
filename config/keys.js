696df2ad06f3d243ab3666da427dd67626ec2683
ZefirTheFear <z4clr07.gaming@gmail.com>
2019-11-18 17:23:56 +0200

    deployment fixing

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // mongoURI: process.env.MONGO_URI,
-  mongoURI:
-    "mongodb+srv://ZefirTheFear:Nerzul969@cluster0-zygzu.mongodb.net/blog2?retryWrites=true",
-  // secretOrKey: process.env.SECRET_OR_KEY
-  secretOrKey: "supersecretkey"
+  mongoURI: process.env.MONGO_URI,
+  // mongoURI:
+  //   "mongodb+srv://ZefirTheFear:Nerzul969@cluster0-zygzu.mongodb.net/blog2?retryWrites=true",
+  secretOrKey: process.env.SECRET_OR_KEY
+  // secretOrKey: "supersecretkey"
 };
